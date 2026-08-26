1e50d4d9525365f553339a3cd82228770e6c5522
赵 <152603147+betechleader@users.noreply.github.com>
2026-08-26 16:19:35 +0800

    Add application entry point

---
+++ src/main/java/com/d5data/backend/BackendHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.d5data.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHelloWorldApplication.class, args);
+    }
+}
+
